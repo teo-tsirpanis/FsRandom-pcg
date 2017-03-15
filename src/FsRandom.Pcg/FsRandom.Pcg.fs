45c087fdad7e5c72482834274f5033fc436fa3c9
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2017-03-15 20:31:15 +0200

    Added code.

--- src/FsRandom.Pcg/FsRandom.Pcg.fs
+++ src/FsRandom.Pcg/FsRandom.Pcg.fs
@@ -3,7 +3,64 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-namespace FsRandom.Pcg
+namespace FsRandom
 
-type Pcg() = 
-    member this.X = "F#"
+open System
+open RandomNumberGenerator
+open Utility
+
+
+[<NoComparison>]
+type PcgState = Pcg of uint64 * uint64
+
+module Pcg =
+
+    [<Literal>]
+    let DefaultMultiplier = 6364136223846793005UL
+    [<Literal>]
+    let DefaultIncrement = 1442695040888963407UL
+
+    let stepState inc state = state * DefaultMultiplier + inc
+
+    let outputPermutation (state: uint64) = 
+        let rotr (value: uint32) (rot: uint32) = 
+            let rot = rot |> int
+            (value >>> rot) ||| (value <<< ((-rot) &&& 31))
+        let state' = ((state >>> 18) ^^^ state) >>> 27
+        let rotValue = state >>> 59 |> uint32
+        rotr (uint32 state) rotValue
+
+    [<CompiledName("PcgRng")>]
+    let pcg x = 
+        let pcg' (Pcg (state, inc)) =
+            let step = stepState inc
+            let getImpl state = 
+                let newState = state |> step
+                let out = state |> outputPermutation
+                out, newState
+            let out1, state1 = getImpl state
+            let out2, state2 = getImpl state1
+            let makeUInt64 (u1: uint32) u2 =
+                let temp = [|u1; u2|] |> Array.collect BitConverter.GetBytes
+                BitConverter.ToUInt64 (temp, 0)
+            (out1, out2) ||> makeUInt64, ((state2, inc) |> Pcg)
+        pcg' x
+        
+    let getInc (Pcg (_, x)) = x
+
+    let setInc newInc (Pcg (state, _)) = (state, newInc) |> Pcg
+
+    let mapInc f (Pcg (state, inc)) = (state, f inc) |> Pcg
+
+    let create seed initSeq = 
+        let mutable state = (0UL, initSeq <<< 1 ||| 1UL) |> Pcg
+        let (Pcg (s, _)) = state |> pcg |> snd
+        state <- ((s + seed), state |> getInc) |> Pcg
+        state <- state |> pcg |> snd
+        state
+
+    let createOneSeq seed = create seed DefaultIncrement
+
+    let createRandom () = 
+        let randomState = createRandomState()
+        (Random.get ruint64 randomState) |> createOneSeq
