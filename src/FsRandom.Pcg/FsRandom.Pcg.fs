2ab79dcf0c962790c22e80bf59f77e2c5157d92f
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2017-03-15 19:38:31 +0200

    Added project structure files; not any code.

---
+++ src/FsRandom.Pcg/FsRandom.Pcg.fs
@@ -0,0 +1,9 @@
+// Copyright (c) 2017 Theodore Tsirpanis
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+namespace FsRandom.Pcg
+
+type Pcg() = 
+    member this.X = "F#"
