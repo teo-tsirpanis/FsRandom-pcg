8180df34abd960197c8ce6832a5b79f6fd695e06
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2017-04-02 18:25:43 +0300

    Refactored Pcg32; it is now smaller and simpler.
    The methods that change the stream index are removed.

--- src/FsRandom.Pcg/Pcg32.fs
+++ src/FsRandom.Pcg/Pcg32.fs
@@ -9,7 +9,7 @@ open System
 
 
 [<NoComparison>]
-type Pcg32State = Pcg of uint64 * uint64
+type Pcg32State = private Pcg of state:uint64 * inc:uint64
 
 module Pcg32 =
 
@@ -17,43 +17,34 @@ module Pcg32 =
     let DefaultMultiplier = 6364136223846793005UL
     [<Literal>]
     let DefaultIncrement = 1442695040888963407UL
+    
+    let getInc (Pcg (_, x)) = x
 
-    let stepState inc state = state * DefaultMultiplier + inc
+    let stepState (Pcg(state, inc)) = state * DefaultMultiplier + inc
+        
+    let private setState (Pcg(_, inc)) newState = (newState, inc) |> Pcg
 
     let outputPermutation (state: uint64) = 
         let rotr (value: uint32) (rot: uint32) = 
             let rot = rot |> int
             (value >>> rot) ||| (value <<< ((-rot) &&& 31))
-        let newState = ((state >>> 18) ^^^ state) >>> 27
+        let newState = ((state >>> 18) ^^^ state) >>> 27 |> uint32
         let rotValue = state >>> 59 |> uint32
-        rotr (uint32 newState) rotValue
+        rotr newState rotValue
 
-    let get x = 
-        let pcg' (Pcg (state, inc)) =
-            let step = stepState inc
-            let getImpl state = 
-                let newState = state |> step
-                let out = state |> outputPermutation
-                out, newState
-            let out1, state1 = getImpl state
-            let out2, state2 = getImpl state1
-            let makeUInt64 (u1: uint32) u2 =
-                let temp = [|u1; u2|] |> Array.collect BitConverter.GetBytes
-                BitConverter.ToUInt64 (temp, 0)
-            (out1, out2) ||> makeUInt64, ((state2, inc) |> Pcg)
-        pcg' x
-        
-    let getInc (Pcg (_, x)) = x
-
-    let setInc newInc (Pcg (state, _)) = (state, newInc) |> Pcg
-
-    let mapInc f (Pcg (state, inc)) = (state, f inc) |> Pcg
+    let get state =
+        let getImpl state = 
+            let newState = state |> stepState |> (setState state)
+            let (Pcg(oldState, _)) = state
+            oldState |> outputPermutation, newState
+        let out1, state1 = getImpl state
+        let out2, state2 = getImpl state1
+        let out = (uint64 out1 <<< 64) + uint64 out2
+        out, state2        
 
     let create seed initSeq = 
-        let mutable state = (0UL, initSeq <<< 1 ||| 1UL) |> Pcg
-        let (Pcg (s, _)) = state |> get |> snd
-        state <- ((s + seed), state |> getInc) |> Pcg
-        state <- state |> get |> snd
-        state
+        let inc = (initSeq <<< 1) ||| 1UL
+        let makeState seed = (seed, inc) |> Pcg
+        makeState 0UL |> stepState
 
     let createOneSeq seed = create seed DefaultIncrement
