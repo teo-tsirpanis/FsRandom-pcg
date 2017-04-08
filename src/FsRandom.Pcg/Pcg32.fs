b28fe40e98cf2341c57db6d433669fcd0dbaf1ee
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2017-04-08 17:50:18 +0300

    Added XML documentation
    The build task now builds in the release mode.

--- src/FsRandom.Pcg/Pcg32.fs
+++ src/FsRandom.Pcg/Pcg32.fs
@@ -1,51 +1,52 @@
 // Copyright (c) 2017 Theodore Tsirpanis
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 namespace FsRandom
 
-open System
-
-
 [<NoComparison>]
+/// A PCG-32 state. It consists of two unsigned 64-bit numbers.
 type Pcg32State = private Pcg of state:uint64 * inc:uint64
 
+/// Functions to deal with `Pcg32State`s.
 module Pcg32 =
 
     [<Literal>]
-    let DefaultMultiplier = 6364136223846793005UL
+    let private DefaultMultiplier = 6364136223846793005UL
     [<Literal>]
-    let DefaultIncrement = 1442695040888963407UL
-    
-    let getInc (Pcg (_, x)) = x
+    let private DefaultIncrement = 1442695040888963407UL
 
-    let stepState (Pcg(state, inc)) = state * DefaultMultiplier + inc
-        
+    let private stepState (Pcg(state, inc)) = state * DefaultMultiplier + inc
+
     let private setState (Pcg(_, inc)) newState = (newState, inc) |> Pcg
 
-    let outputPermutation (state: uint64) = 
-        let rotr (value: uint32) (rot: uint32) = 
+    let private outputPermutation (state: uint64) =
+        let rotr (value: uint32) (rot: uint32) =
             let rot = rot |> int
             (value >>> rot) ||| (value <<< ((-rot) &&& 31))
         let newState = ((state >>> 18) ^^^ state) >>> 27 |> uint32
         let rotValue = state >>> 59 |> uint32
         rotr newState rotValue
 
-    let get state =
-        let getImpl state = 
-            let newState = state |> stepState |> (setState state)
-            let (Pcg(oldState, _)) = state
-            oldState |> outputPermutation, newState
-        let out1, state1 = getImpl state
-        let out2, state2 = getImpl state1
-        let out = (uint64 out1 <<< 64) + uint64 out2
-        out, state2        
+    /// Gets the stream index of a state.
+    let getInc (Pcg (_, x)) = x
 
-    let create seed initSeq = 
+    /// Generates a random number from theprovided state.
+    /// Returns a tuple consisting of the new number and the new state.
+    let get state =
+        let newState = state |> stepState |> (setState state)
+        let (Pcg(oldState, _)) = state
+        oldState |> outputPermutation, newState
+
+    /// Creates a PCG-32 state from the given seed and stream index.
+    let create seed initSeq =
         let inc = (initSeq <<< 1) ||| 1UL
         let makeState seed = (seed, inc) |> Pcg
         makeState 0UL |> stepState |> ((+) seed)
                       |> makeState |> stepState |> makeState
 
+    /// Creates a PCG-32 state from the given seed.
+    /// If you don't care about the stream index,
+    /// it's better to call this, instead of `create seed 0`.
     let createOneSeq seed = create seed DefaultIncrement
--- src/FsRandom.Pcg/Pcg64.fs
+++ src/FsRandom.Pcg/Pcg64.fs
@@ -1,5 +1,5 @@
 // Copyright (c) 2017 Theodore Tsirpanis
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
@@ -7,57 +7,80 @@ namespace FsRandom
 
 open System
 
+/// An unsigned 128-bit number.
+/// It must be converted to a bigint in to access its value.
 type UInt128 = private UInt128 of bigint
 with
     override x.ToString() = match x with | UInt128 x -> x.ToString()
 
 [<AutoOpen>]
+/// Functions to deal with `UInt128`s.
 module UInt128 =
 
+    /// Number two raised in the 128th power, _as a `bigint`_.
     let ``2^128`` = 340282366920938463463374607431768211456I
 
+    /// Converts a `bigint` to a `UInt128`.
+    /// The absolute value of the bigint mod 2^128
+    /// will be the value of the new `UInt128`.
     let uint128 (x: bigint) = x % ``2^128`` |> abs |> UInt128
 
+    /// Converts a `UInt128` to a `bigint`.
     let bigint (UInt128 x) = x
 
+    /// Applies a function to the value of a `UInt128`.
+    /// A shortcut of calling `x |> bigint |> f`.
     let apply f (UInt128 x) = f x
 
+    /// Applies a function that processes and returns a bigint
+    /// to the value of a `UInt128`, and then converts it to a new `UInt128`.
+    /// Useful for making operations with `UInt128`s.
+    /// A shortcut of calling 'x |> apply f |> uint128'
     let map f x = (f, x) ||> apply |> uint128
 
+    /// Number zero as a `UInt128`.
     let zero = 0I |> UInt128
 
 [<NoComparison>]
+/// A PCG-64 state. It consists of two `UInt128`s.
 type Pcg64State = private Pcg of state:UInt128 * inc:UInt128
 
 module Pcg64 =
 
-    let defaultMultiplier = 47026247687942121848144207491837523525I
+    let private defaultMultiplier = 47026247687942121848144207491837523525I
 
-    let defaultIncrement = 117397592171526113268558934119004209487I
-
-    let getInc (Pcg(_, inc)) = inc
+    let private defaultIncrement = 117397592171526113268558934119004209487I
 
     let private setState (Pcg(_, inc)) newState = (newState, inc) |> Pcg
 
-    let stepState (Pcg(UInt128(inc), UInt128(state))) = state * defaultMultiplier + inc |> uint128
+    let private stepState (Pcg(UInt128(inc), UInt128(state))) = state * defaultMultiplier + inc |> uint128
 
-    let outputPermutation (UInt128 state) = 
-        let rotr (value: uint64) (rot: uint32) = 
+    let private outputPermutation (UInt128 state) =
+        let rotr (value: uint64) (rot: uint32) =
             let rot = rot |> int
             (value >>> rot) ||| (value <<< ((-rot) &&& 63))
         let state' = (state >>> 64) ^^^ state |> uint64
         let rotValue = state >>> 122 |> uint32
         rotr state' rotValue
-    
+
+    /// Gets the stream index of a state.
+    let getInc (Pcg(_, inc)) = inc
+
+    /// Generates a random number from theprovided state.
+    /// Returns a tuple consisting of the new number and the new state.
     let get state =
         let state = state |> stepState |> (setState state)
         let (Pcg(state', _)) = state
         state' |> outputPermutation, state
 
+    /// Creates a PCG-64 state from the given seed and stream index.
     let create seed initSeq =
         let inc = (initSeq <<< 1) ||| 1I |> uint128
         let makeState seed = (seed, inc) |> Pcg
-        makeState zero |> stepState |> (map ((+) seed)) 
+        makeState zero |> stepState |> (map ((+) seed))
                        |> makeState |> stepState |> makeState
 
+    /// Creates a PCG-64 state from the given seed.
+    /// If you don't care about the stream index,
+    /// it's better to call this, instead of `create seed 0`.
     let createOneSeq seed = create seed defaultIncrement
