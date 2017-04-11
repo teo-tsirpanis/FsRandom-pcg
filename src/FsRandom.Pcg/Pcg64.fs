f75cc861b1c0616e0cad96ec4bdd0422fe98f248
Theodore Tsirpanis <teo.tsirpanis.718@gmail.com>
2017-04-11 12:24:10 +0300

    Added an optimized 3rd-party UInt128 implementation.

--- src/FsRandom.Pcg/Pcg64.fs
+++ src/FsRandom.Pcg/Pcg64.fs
@@ -5,62 +5,68 @@
 
 namespace FsRandom
 
+open SoftWx.Numerics
 open System
-
-/// An unsigned 128-bit number.
-/// It must be converted to a bigint in to access its value.
-type UInt128 = private UInt128 of bigint
-with
-    override x.ToString() = match x with | UInt128 x -> x.ToString()
+open System.Numerics
+open System.Security.Cryptography
 
 [<AutoOpen>]
 /// Functions to deal with `UInt128`s.
 module UInt128 =
 
+    /// Returns the low part of a `UInt128`.
+    let low (x: UInt128) = x.Low
+
+    /// Returns the hogh part of a `UInt128`.
+    let high (x:UInt128) = x.High
+
     /// Number two raised in the 128th power, _as a `bigint`_.
     let ``2^128`` = 340282366920938463463374607431768211456I
+
+    /// Converts a `UInt128` to a `bigint`.
+    let bigint x =
+        let lo, hi = x |> low |> bigint, x |> high |> bigint
+        (hi <<< 64) + lo
 
     /// Converts a `bigint` to a `UInt128`.
     /// The absolute value of the bigint mod 2^128
     /// will be the value of the new `UInt128`.
-    let uint128 (x: bigint) = x % ``2^128`` |> abs |> UInt128
+    let uint128 x =
+        let x = (abs x) % ``2^128``
+        let bytes = x.ToByteArray() |> Array.rev
+        let f i = BitConverter.ToUInt64(bytes, i)
+        (f 0, f 8) |> UInt128
 
-    /// Converts a `UInt128` to a `bigint`.
-    let bigint (UInt128 x) = x
-
-    /// Applies a function to the value of a `UInt128`.
-    /// A shortcut of calling `x |> bigint |> f`.
-    let apply f (UInt128 x) = f x
-
-    /// Applies a function that processes and returns a bigint
-    /// to the value of a `UInt128`, and then converts it to a new `UInt128`.
-    /// Useful for making operations with `UInt128`s.
-    /// A shortcut of calling 'x |> apply f |> uint128'
-    let map f x = (f, x) ||> apply |> uint128
-
-    /// Number zero as a `UInt128`.
-    let zero = 0I |> UInt128
+    /// Returns a cryptographically random `UInt128`.
+    /// Useful for seeding PCG-64.
+    let random () =
+        let random64 () =
+            let rng = RandomNumberGenerator.Create()
+            let bytes = Array.zeroCreate 8
+            rng.GetBytes bytes
+            BitConverter.ToUInt64 (bytes, 0)
+        UInt128 (random64(), random64())
 
 [<NoComparison>]
 /// A PCG-64 state. It consists of two `UInt128`s.
-type Pcg64State = private Pcg of state:UInt128 * inc:UInt128
+type Pcg64State = Pcg of state:UInt128 * inc:UInt128
 
 module Pcg64 =
 
-    let private defaultMultiplier = 47026247687942121848144207491837523525I
+    let private defaultMultiplier = UInt128(2549297995355413924UL, 4865540595714422341UL)
 
-    let private defaultIncrement = 117397592171526113268558934119004209487I
+    let private defaultIncrement = UInt128(6364136223846793005UL, 1442695040888963407UL)
 
     let private setState (Pcg(_, inc)) newState = (newState, inc) |> Pcg
 
-    let private stepState (Pcg(UInt128(inc), UInt128(state))) = state * defaultMultiplier + inc |> uint128
+    let private stepState (Pcg(inc, state)) = state * defaultMultiplier + inc
 
-    let private outputPermutation (UInt128 state) =
+    let private outputPermutation (state: UInt128) =
         let rotr (value: uint64) (rot: uint32) =
             let rot = rot |> int
             (value >>> rot) ||| (value <<< ((-rot) &&& 63))
-        let state' = (state >>> 64) ^^^ state |> uint64
-        let rotValue = state >>> 122 |> uint32
+        let state' = state.High ^^^ state.Low |> uint64
+        let rotValue = state >>> 122 |> uint64 |> uint32
         rotr state' rotValue
 
     /// Gets the stream index of a state.
@@ -75,9 +81,9 @@ module Pcg64 =
 
     /// Creates a PCG-64 state from the given seed and stream index.
     let create seed initSeq =
-        let inc = (initSeq <<< 1) ||| 1I |> uint128
+        let inc = (initSeq <<< 1) ||| UInt128.One
         let makeState seed = (seed, inc) |> Pcg
-        makeState zero |> stepState |> (map ((+) seed))
+        makeState UInt128.Zero |> stepState |> ((+) seed)
                        |> makeState |> stepState |> makeState
 
     /// Creates a PCG-64 state from the given seed.
